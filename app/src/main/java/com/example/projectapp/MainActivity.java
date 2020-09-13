357dcee328cd71a1c3992ab244b9500d3626f67f
kamalagbaria <62603884+kamalagbaria@users.noreply.github.com>
2020-09-13 20:52:22 +0300

    Removing Action Bar
    Removing the Action bar from the main activity

--- app/src/main/java/com/example/projectapp/MainActivity.java
+++ app/src/main/java/com/example/projectapp/MainActivity.java
@@ -33,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController= Navigation.findNavController(this,R.id.navHostFragment);
         NavigationUI.setupWithNavController(navigationView,navController);
 
-
     }
 
     @Override
